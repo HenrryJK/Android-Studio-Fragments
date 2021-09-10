fd516909a22c3854fa760377461a3139b69c7e34
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-09-09 22:31:58 -0500

    Trabajo terminado

---
+++ app/src/main/java/com/example/fragments/fragmentss/FragmentA.kt
@@ -0,0 +1,28 @@
+package com.example.fragments.fragmentss
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragments.Communicator
+import com.example.fragments.R
+import kotlinx.android.synthetic.main.fragment_a.view.*
+
+class FragmentA : Fragment() {
+        private lateinit var communicator: Communicator
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view  = inflater.inflate(R.layout.fragment_a, container, false)
+
+        communicator = activity as Communicator
+    view.sendBtn.setOnClickListener {
+        communicator.passDataCom(view.messageInput.text.toString())
+    }
+        return view
+    }
+    }
